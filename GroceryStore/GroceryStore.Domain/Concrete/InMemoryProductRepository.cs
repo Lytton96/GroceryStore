365c422a829b4b77fe135bd2c9d8e5b353d2a0e8
YL <YL>
2020-11-26 20:55:17 -0500

    1 [CHG} add repositories;

---
+++ GroceryStore/GroceryStore.Domain/Concrete/InMemoryProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using GroceryStore.Domain.Abstract;
+using GroceryStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Domain.Concrete
+{
+    class InMemoryProductRepository : IProductsRepository
+    {
+        private List<Product> _products = new List<Product>
+         {
+             new Product{ Name = "Eggplant", Price = 1},
+             new Product {Name ="Pepper", Price = 4},
+             new Product {Name = "Cucumber", Price = 2}
+         };
+
+        public IEnumerable<Product> Products
+        {
+            get { return _products; }
+        }
+    }
+}
