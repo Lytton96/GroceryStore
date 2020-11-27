479b61002b53d715723ce496f6b70fa366881c1f
YL <YL>
2020-11-27 12:54:06 -0500

    1 [CHG} add data to database;

--- GroceryStore/GroceryStore.DebugConsole/Program.cs
+++ GroceryStore/GroceryStore.DebugConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GroceryStore.Domain.Concrete;
+using GroceryStore.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,27 @@ namespace GroceryStore.DebugConsole
     {
         static void Main(string[] args)
         {
+            using ( var ctx = new EFDbContext())
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var product = new Product()
+                    {
+                        Name = $"Breakfast Pork _{i}",
+                        Price = 6.5m,
+                        Description = "This is pork",
+                        Category = "Meat"
+                    };
+
+                    ctx.Products.Add(product);
+                }
+                
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
         }
     }
 }
---
+++ GroceryStore/GroceryStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GroceryStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Domain.Concrete
+{
+    public class EFDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
