8caa0d8f13a875bf4ccb882815e09765c2b8d30a
YL <YL>
2020-11-29 09:57:41 -0500

    1 [CHG] add new menus;

---
+++ GroceryStore/GroceryStore.WebApp/Controllers/NavController.cs
@@ -0,0 +1,27 @@
+﻿using GroceryStore.Domain.Abstract;
+using GroceryStore.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroceryStore.WebApp.Controllers
+{
+    public class NavController : Controller
+    {
+        public IProductsRepository ProductsRepository { get; set; }
+      = new EFProductRepository();
+        // GET: Nav
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = ProductsRepository
+                .Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+    }
+}
