0c1e68a139c4776dd539c247c0d0e426354b631c
YL <YL>
2020-11-29 11:49:00 -0500

    1 [CHG] refine cart;

--- GroceryStore/GroceryStore.WebApp/Controllers/CartController.cs
+++ GroceryStore/GroceryStore.WebApp/Controllers/CartController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GroceryStore.Domain.Abstract;
+using GroceryStore.Domain.Concrete;
+using GroceryStore.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,45 @@ namespace GroceryStore.WebApp.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
-        public ActionResult Index()
+        public IProductsRepository ProductsRepository { get; set; }
+= new EFProductRepository();
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
         {
-            return View();
+            Product product = ProductsRepository
+            .Products
+            .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = ProductsRepository
+            .Products
+            .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart(); Session["Cart"] = cart;
+            }
+
+            return cart;
         }
     }
+
 }
